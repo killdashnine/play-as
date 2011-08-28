c97d13a0c2a4d6be52b932ad7c3ad24d3a8f9298
Matthias van der Vlies <mvdvlies@gmail.com>
2011-08-28 22:27:54 +0200

    #9 show application log
    Signed-off-by: Matthias van der Vlies <mvdvlies@gmail.com>

--- app/controllers/LogController.java
+++ app/controllers/LogController.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2011 Matthias van der Vlies
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package controllers;
 
 import java.io.IOException;
-import java.util.concurrent.Future;
 
+import models.Application;
 import play.Logger;
 import play.libs.F.Promise;
 import play.mvc.WebSocketController;
@@ -10,15 +26,15 @@ import core.LogGenerator;
 
 public class LogController extends WebSocketController {
 
-	/*public static void application(final Long id) {
+	public static void application(final Long id) throws IOException, Exception {
 		final Application application = Application.findById(id);
 		if(application == null) {
 			disconnect();
 		}
 		else {
-			
+			logToOutbound("logs/" + application.pid + ".log");
 		}
-	}*/
+	}
 	
 	public static void manager() throws Exception {
 		logToOutbound("logs/play-as.log");
--- app/core/LogGenerator.java
+++ app/core/LogGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Matthias van der Vlies
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package core;
 
 import java.io.BufferedReader;
@@ -20,7 +36,8 @@ public class LogGenerator extends Job {
 		fileInputStream = new FileInputStream (filePath);
 		inputStreamReader = new InputStreamReader(fileInputStream, "utf-8");
 		bufferedReader = new BufferedReader(inputStreamReader);
-		bufferedReader.skip(fileInputStream.available()); // skip to end
+		//bufferedReader.skip(fileInputStream.available()); // skip to end
+		// NOTE: We need to skip some, but not all
 	}
 	
 	public String doJobWithResult() throws Exception {
